4a0c71375f6691cc4f83af8ac83feb10cfb7dcac
AnnieeeeeF <c7fan@ucsd.edu>
2023-02-21 17:18:19 -0800

    updated plot layout

--- plot.py
+++ plot.py
@@ -53,7 +53,7 @@ def create_plots(df):
         y=df['AirTemp_Avg'],
         mode='markers',
         showlegend=True,
-        name='Air Temperature')
+        name='Air Temp')
         )
 
     fig4.add_trace(go.Scatter(
@@ -65,14 +65,14 @@ def create_plots(df):
         )
 
     fig1.update_xaxes(title_text='time(local)')
-    fig2.update_xaxes(title_text='time(local)')
+    fig2.update_xaxes(title_text='time(local)', gridcolor='lightgrey')
     fig3.update_xaxes(title_text='time(local)')
-    fig4.update_xaxes(title_text='time(local)')
+    fig4.update_xaxes(title_text='time(local)', linecolor='black', mirror=True, ticks='outside', showline=True)
 
     fig1.update_yaxes(title_text="$W/m^2$")
-    fig2.update_yaxes(title_text="$W/m^2$")
+    fig2.update_yaxes(title_text="$W/m^2$", gridcolor='lightgrey')
     fig3.update_yaxes(title_text="Air Temp")
-    fig4.update_yaxes(title_text="RH")
+    fig4.update_yaxes(title_text="RH", linecolor='black', mirror=True, ticks='outside', showline=True)
 
     fig1.update_traces(marker=dict(size=5))
     fig2.update_traces(marker=dict(size=5))
@@ -80,7 +80,9 @@ def create_plots(df):
     fig4.update_traces(marker=dict(size=5))
 
     fig1.update_layout(title='GHI', yaxis_range=[0, 1200])
-    fig2.update_layout(title='DNI', yaxis_range=[0, 1200])
+    fig2.update_layout(title='DNI', yaxis_range=[0, 1200], plot_bgcolor='white')
+    fig3.update_layout(title='Air Temperature')
+    fig4.update_layout(title='RH', plot_bgcolor='white')
 
     filename1 = os.path.join("html_plots", "ghi_plot.html")
     filename2 = os.path.join("html_plots", "dni_plot.html")
